771a983181722a19fa3da4694fe6837b7726815a
Tackeri <sd.minter@gmail.com>
2018-11-01 18:52:32 -0500

    Added get fields, layouts, and meta data

---
+++ Python/Zoho_CRM_API/2.0/getFields.py
@@ -0,0 +1,58 @@
+__author__ = 'MinterS'
+
+# Zoho CRM API v2.0 - Get Records from Module
+# This program will fetch 2 records from one of the following modules:
+
+# leads, accounts, contacts, deals,
+# campaigns, tasks, cases, events,
+# calls, solutions, products, vendors,
+# pricebooks, quotes, sales_orders,
+# purchase_orders, invoices, custom, notes,
+# approvals, dashboards, search and activities.
+
+from Authentication import tokens
+from Resources import statusCodes
+import requests, json, os
+
+module = 'Contacts'
+
+# Call the API and Print the Response
+
+
+def getRecords(module):
+    url = 'https://www.zohoapis.com/crm/v2/settings/fields?module={module_id}'
+
+
+    url = url.replace("{module_id}", module)
+    headers = {
+        'Authorization': tokens.getAccess(), 
+        }
+
+    print('')
+    print(url)
+    print('')
+    print(headers)
+    print('')
+    # Call API
+    response = requests.get(url, headers=headers)
+    return response
+
+
+def printResponse(response, module):
+    jsonData = json.loads(response.text)
+    print(json.dumps(jsonData, sort_keys=True, indent=4))
+
+    # fileName = 'output_' + os.path.basename(__file__) + '.json'
+    # outputFile = open(fileName, "w")
+    # json.dump(jsonData, outputFile)
+
+
+# Run Program
+response = getRecords(module)
+if response.status_code == 200:
+    printResponse(response, module)
+else:
+    print('')
+    print(response.status_code)
+    print(statusCodes.statusCode(response.status_code))
+    print(response.text)
---
+++ Python/Zoho_CRM_API/2.0/getLayoutById.py
@@ -0,0 +1,59 @@
+__author__ = 'MinterS'
+
+# Zoho CRM API v2.0 - Get Records from Module
+# This program will fetch 2 records from one of the following modules:
+
+# leads, accounts, contacts, deals,
+# campaigns, tasks, cases, events,
+# calls, solutions, products, vendors,
+# pricebooks, quotes, sales_orders,
+# purchase_orders, invoices, custom, notes,
+# approvals, dashboards, search and activities.
+
+from Authentication import tokens
+from Resources import statusCodes
+import requests
+import json
+
+module = 'Contacts'
+record_id = 'd123123123'
+
+# Call the API and Print the Response
+
+
+def getRecords(module):
+    url = 'https://www.zohoapis.com/crm/v2/settings/layouts/{record_id}?module={module_id}'
+
+    url = url.replace("{module_id}", module)
+    url = url.replace("{record_id}", record_id)
+    headers = {
+        'Authorization': tokens.getAccess(), 
+        }
+
+    print('')
+    print(url)
+    print('')
+    print(headers)
+    print('')
+    # Call API
+    response = requests.get(url, headers=headers)
+    return response
+
+
+def printResponse(response, module):
+    jsonData = json.loads(response.text)
+    print(json.dumps(jsonData, sort_keys=True, indent=4))
+
+    # outputFile = open("test.json", "w")
+    # json.dump(jsonData, outputFile)
+
+
+# Run Program
+response = getRecords(module)
+if response.status_code == 200:
+    printResponse(response, module)
+else:
+    print('')
+    print(response.status_code)
+    print(statusCodes.statusCode(response.status_code))
+    print(response.text)
