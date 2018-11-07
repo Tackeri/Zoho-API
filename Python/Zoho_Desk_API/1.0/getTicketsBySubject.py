7db2728d8f41fd5bec96c71683aa23dfcdffb7fd
Tackeri <sd.minter@gmail.com>
2018-11-07 15:45:10 -0600

    Added desk ticket subject search

---
+++ Python/Zoho_Desk_API/1.0/getTicketsBySubject.py
@@ -0,0 +1,44 @@
+__author__ = 'MinterS'
+
+# Zoho Desk API v1.0 - Get Organizations from Desk
+# This program will fetch the list of Zoho Desk organizations.
+
+from Authentication import tokens
+from Resources import statusCodes
+import requests, json
+
+organizationId = '673932772'
+searchString = 'Hello!'
+
+# Call the API and Print the Response
+
+def getRecords(organizationId):
+    url = 'https://desk.zoho.com/api/v1/tickets/search?subject={searchString}'
+    url = url.replace("{searchString}", searchString)
+    headers = {
+        'orgId': organizationId,
+        'Authorization': tokens.getAccess(),
+    }
+    print('')
+    print(url)
+    print('')
+    print(headers)
+    print('')
+    # Call API
+    response = requests.get(url, headers=headers)
+    return response
+
+def printResponse(response):
+    jsonData = json.loads(response.text)
+    data = jsonData
+    print(json.dumps(data, sort_keys=True, indent=4))
+
+# Run Program
+response = getRecords(organizationId)
+if response.status_code == 200:
+    printResponse(response)
+else:
+    print('')
+    print(response.status_code)
+    print(statusCodes.statusCode(response.status_code))
+    print(response.text)
