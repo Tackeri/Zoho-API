77a321a8215660d9466398522ae66100e5692fde
georgeotj <george.huerta42@gmail.com>
2019-03-08 13:57:34 -0600

    Add files via upload
    These scripts will allow user's to take Mockaroo schemas to automatically generate mock data into the CRM through an API

---
+++ Python/Zoho_CRM_API/2.0/upsertLeadsMockaroo.py
@@ -0,0 +1,44 @@
+__author__ = 'JorgeH'
+
+import requests
+import json
+import urllib.request
+from Authentication import tokens
+
+# Define URL
+url = 'https://www.zohoapis.com/crm/v2/Leads/upsert'
+urlMockaroo = 'https://my.api.mockaroo.com/zcrmLCADT.json?key=58fd1ee0'
+
+# Reads the Mockaroo data and loads it into the file
+data = urllib.request.urlopen(urlMockaroo).read()
+mockData = json.loads(data)
+
+# Get Token and Headers
+token = tokens.getAccess()
+headers = {'Authorization': token}
+
+# Build Body
+# NOTE: The mockData[x] must be iterated through. Currently working on this.
+body = {
+    "data": [
+       mockData[0], mockData[1], mockData[2]
+    ]
+}
+
+# Print Input Data
+print(' \n ---- Request ----')
+print('URL:', url)
+print('Head:', headers)
+
+# Call API
+response = requests.post(url, headers=headers, json=body)
+
+# Print Output Data
+if (response.status_code) == 200:
+    jsonData = json.loads(response.text)
+    data = jsonData['data']
+    print(' \n ---- Response ----')
+    print(json.dumps(data, sort_keys=True, indent=4))
+else:
+    print(response.status_code)
+    print(response.text)
