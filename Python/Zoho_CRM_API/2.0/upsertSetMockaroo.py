5f81aee6d4856d4525311594dc36cb96dc6d9e5e
georgeotj <george.huerta42@gmail.com>
2019-09-05 10:53:15 -0500

    Update upsertSetMockaroo.py
    Reorganized the API calls so that a bug does not affect the script.

--- Python/Zoho_CRM_API/2.0/upsertSetMockaroo.py
+++ Python/Zoho_CRM_API/2.0/upsertSetMockaroo.py
@@ -1,9 +1,10 @@
-import requests
-import json
-import urllib.request
+import requests, json, urllib.request
 import getAuthRefresh
-from Authentication import tokens, credentials
-from Resources import statusCodes
+from Authentication import tokens
+
+# Necessary URLs
+url ='https://www.zohoapis.com/crm/v2/{module}/upsert'
+urlMockaroo =''
 
 def upsertRecords(url, headers, body, getAuthRefresh):
     # Print Input Data
@@ -15,22 +16,18 @@ def upsertRecords(url, headers, body, getAuthRefresh):
     response = requests.post(url, headers=headers, json=body)
 
     # Print Output Data
-    if (response.status_code) == 200:
+    if response.status_code == 200:
         jsonData = json.loads(response.text)
         data = jsonData['data']
         print(' \n ---- Response ----')
         print(json.dumps(data, sort_keys=True, indent=4))
-    elif (response.status_code) == 401:
+    elif response.status_code == 401:
         print(response.status_code)
         print(response.text)
         runAgain = getAuthRefresh
     else:
         print(response.status_code)
         print(response.text)
-
-# Necessary URLs
-url = 'https://www.zohoapis.com/crm/v2/{module}/upsert'
-urlMockaroo = 'https://my.api.mockaroo.com/zohopresalesmegaschema.json?key=58fd1ee0'
 
 # Reads the Mockaroo data and loads it into the file
 data = urllib.request.urlopen(urlMockaroo).read()
@@ -44,21 +41,24 @@ headers = {'Authorization': token}
 # NOTE: The mockData[x] must be iterated through, still haven't been able to figure it out.
 body = {
     "data": [
-        mockData[0], mockData[1], mockData[2], mockData[3], mockData[4]
+        mockData[0]
     ]
 }
 
+# Upsert to the Properties module
 url = url.replace('{module}', 'Properties')
 upsertRecords(url, headers, body, getAuthRefresh)
 url = url.replace('Properties', '{module}')
 
-url = url.replace('{module}', 'Contacts')
-upsertRecords(url, headers, body, getAuthRefresh)
-url = url.replace('Contacts', '{module}')
-
+# Upsert to the Accounts module
 url = url.replace('{module}', 'Accounts')
 upsertRecords(url, headers, body, getAuthRefresh)
 url = url.replace('Accounts', '{module}')
+
+# Upsert to the Contacts module
+url = url.replace('{module}', 'Contacts')
+upsertRecords(url, headers, body, getAuthRefresh)
+url = url.replace('Contacts', '{module}')
 
 url = url.replace('{module}', 'Deals')
 upsertRecords(url, headers, body, getAuthRefresh)
